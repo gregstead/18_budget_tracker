92af3814418f7e11f6d87778362e61066ed834da
Greg Stead <gregstead@gmail.com>
2021-02-06 22:13:03 -0600

    adds server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+// express configurations
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// mongoose connection
+mongoose.connect("mongodb://localhost/budget", {
+  userNewUrlParser: true,
+  userFindAndModify: false,
+});
+
+//routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
